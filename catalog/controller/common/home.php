e57c7e9855f90b906b5ee4fd06872b3549e965d6
Kudenkov Roman <kudenkovr@gmail.com>
2020-03-25 17:03:00 +0300

    Fix home page slideshow

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -11,14 +11,14 @@ class ControllerCommonHome extends Controller {
 
 		// $data['column_left'] = $this->load->controller('common/column_left');
 		// $data['column_right'] = $this->load->controller('common/column_right');
-		// $data['content_top'] = $this->load->controller('common/content_top');
+		$data['content_top'] = $this->load->controller('common/content_top');
 		// $data['content_bottom'] = $this->load->controller('common/content_bottom');
 		
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 		
 		$data['menu'] = $this->load->controller('common/menu');
-		$data['products_list'] = $this->load->controller('product/products_list');
+		// $data['products_list'] = $this->load->controller('product/products_list');
 		
 
 		$this->response->setOutput($this->load->view('common/home', $data));
