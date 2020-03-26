1e5423b4235c7300d7f44a85b09bc6fd46e0d63f
Kudenkov Roman <kudenkovr@gmail.com>
2020-03-26 23:43:18 +0300

    Create product and products-list costrat

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -441,6 +441,7 @@ class ControllerProductProduct extends Controller {
 			$data['content_bottom'] = $this->load->controller('common/content_bottom');
 			$data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
+			$data['menu'] = $this->load->controller('common/menu');
 
 			$this->response->setOutput($this->load->view('product/product', $data));
 		} else {
